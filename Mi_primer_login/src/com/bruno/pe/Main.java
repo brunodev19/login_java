483779bf493f9cc4775d2cf0e294802b31a3b803
Mr Jones <brunodev19@gmail.com>
2021-06-02 08:36:58 -0500

    Incluyendo Scanner

--- Mi_primer_login/src/com/bruno/pe/Main.java
+++ Mi_primer_login/src/com/bruno/pe/Main.java
@@ -1,10 +1,18 @@
 package com.bruno.pe;
 
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Ingrese su Usuario:");
+        String usuarioIngresado = miScanner.nextLine();
+        System.out.println("Ingrese su Password");
+        String passwordIngresado = miScanner.nextLine();
+
         Login miLogin = new Login();
-        
-        miLogin.comprobarCredenciales("BRUNO", "abcd");
+
+        miLogin.comprobarCredenciales(usuarioIngresado, passwordIngresado);
     }
 }
